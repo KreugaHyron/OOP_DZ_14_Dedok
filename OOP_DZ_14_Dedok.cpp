ac6d364863212a1d9a9a3ed3211e4e4b014cfcbf
DXdok_zp32@student.itstep.org <DXdok_zp32@student.itstep.org>
2024-02-03 16:45:48 +0200

    Добавьте файлы проекта.

---
+++ OOP_DZ_14_Dedok.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+template <typename T>
+class Swapper {
+public:
+    static void Swap(T& a, T& b) {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+};
+int main() {
+    int num1 = 5, num2 = 10;
+    cout << "Before swapping: num1 = " << num1 << ", num2 = " << num2 << "\n";
+
+    Swapper<int>::Swap(num1, num2);
+
+    cout << "After swapping: num1 = " << num1 << ", num2 = " << num2 << "\n";
+
+    return 0;
+}
